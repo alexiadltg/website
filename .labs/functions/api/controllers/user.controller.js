bf2e946aba69eab3617d74e9f4833ad087ef2da7
Easel7231 <a22yanzhayan@inspedralbes.cat>
2023-02-24 10:57:52 +0100

    API get all working

--- .labs/functions/api/controllers/user.controller.js
+++ .labs/functions/api/controllers/user.controller.js
@@ -1,3 +1,8 @@
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb://127.0.0.1:27017/test'; 
+const client = new MongoClient(uri);
+
 exports.allAccess = (req, res) => {
   res.status(200).send("Public Content.");
 };
@@ -6,6 +11,17 @@ exports.userBoard = (req, res) => {
   res.status(200).send("User Content.");
 };
 
-exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
+exports.adminBoard = async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('test');
+    const usersCollection = database.collection('users');
+    const users = await usersCollection.find().toArray();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  } finally {
+    await client.close();
+  }
 };
