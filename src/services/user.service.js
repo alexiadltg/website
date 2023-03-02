0896108023f4a899c3c49d9ed53793380f87ff49
alexiadltg <a22aledelgar@inspedralbes.cat>
2023-03-02 11:41:23 +0100

    env

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,23 +1,23 @@
-import axios from 'axios';
-import authHeader from './auth-header';
+import axios from "axios";
+import authHeader from "./auth-header";
 
-const API_URL = 'http://127.0.0.1:6869/api/test/';
+const API_URL = process.env.API_URL || "http://127.0.0.1:6869/api/test/";
 
 class UserService {
   getPublicContent() {
-    return axios.get(API_URL + 'all');
+    return axios.get(API_URL + "all");
   }
 
   getUserBoard() {
-    return axios.get(API_URL + 'user', { headers: authHeader() });
+    return axios.get(API_URL + "user", { headers: authHeader() });
   }
 
   getModeratorBoard() {
-    return axios.get(API_URL + 'mod', { headers: authHeader() });
+    return axios.get(API_URL + "mod", { headers: authHeader() });
   }
 
   getAdminBoard() {
-    return axios.get(API_URL + 'admin', { headers: authHeader() });
+    return axios.get(API_URL + "admin", { headers: authHeader() });
   }
 }
 
