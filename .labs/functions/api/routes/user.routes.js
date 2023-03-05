f5d386ef15739cf78775131a0343f99ac4120918
Easel7231 <a22yanzhayan@inspedralbes.cat>
2023-03-05 13:09:56 +0100

    new user routes

--- .labs/functions/api/routes/user.routes.js
+++ .labs/functions/api/routes/user.routes.js
@@ -11,12 +11,12 @@ module.exports = function(app) {
     next();
   });
 
-  app.get("/api/test/all", controller.allAccess);
+  app.get("/api/public/landing", controller.allAccess);
 
-  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
+  app.get("/api/user/userboard", [authJwt.verifyToken], controller.userBoard);
 
   app.get(
-    "/api/test/admin",
+    "/api/admin/adminboard",
     [authJwt.verifyToken, authJwt.isAdmin],controller.adminBoard
   );
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = process.env.API_URL || "http://127.0.0.1:6869/api/test/";
+const API_URL = process.env.API_URL || "http://127.0.0.1:6869/api/";
 
 class UserService {
   getPublicContent() {
-    return axios.get(API_URL + "all");
+    return axios.get(API_URL + "public/landing");
   }
 
   getUserBoard() {
-    return axios.get(API_URL + "user", { headers: authHeader() });
+    return axios.get(API_URL + "user/userboard", { headers: authHeader() });
   }
 
   getAdminBoard() {
-    return axios.get(API_URL + "admin", { headers: authHeader() });
+    return axios.get(API_URL + "admin/adminboard", { headers: authHeader() });
   }
 }
 
