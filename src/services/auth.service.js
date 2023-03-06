79a311cfcaa749fa92194effc5065bef079cb7d5
alexiadltg <a22aledelgar@inspedralbes.cat>
2023-03-06 08:58:13 +0100

    fix

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,17 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://127.0.0.1:6869/api/auth/';
+const API_URL =
+  process.env.API_URL + "/auth" || "http://127.0.0.1:6869/api/auth/";
 
 class AuthService {
   login(user) {
     return axios
-      .post(API_URL + 'signin', {
+      .post(API_URL + "signin", {
         username: user.username,
-        password: user.password
+        password: user.password,
       })
-      .then(response => {
+      .then((response) => {
         if (response.data.accessToken) {
-          localStorage.setItem('user', JSON.stringify(response.data));
+          localStorage.setItem("user", JSON.stringify(response.data));
         }
 
         return response.data;
@@ -19,14 +20,14 @@ class AuthService {
   }
 
   logout() {
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
   }
 
   register(user) {
-    return axios.post(API_URL + 'signup', {
+    return axios.post(API_URL + "signup", {
       username: user.username,
       email: user.email,
-      password: user.password
+      password: user.password,
     });
   }
 }
