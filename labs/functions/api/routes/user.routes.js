4394e5b7e135a13de8f65e82324604c769252114
Easel7231 <a22yanzhayan@inspedralbes.cat>
2023-03-08 19:20:02 +0100

    verify before check admin

--- labs/functions/api/routes/user.routes.js
+++ labs/functions/api/routes/user.routes.js
@@ -22,7 +22,7 @@ module.exports = function (app) {
   
   
   app.post('/api/user/newgame', [authJwt.verifyToken], controller.newGame);
-  app.post('/api/admin/newconfig', [authJwt.isAdmin],controller.newConfig)
+  app.post('/api/admin/newconfig', [authJwt.verifyToken, authJwt.isAdmin],controller.newConfig)
 
-  app.put('/api/admin/putUserAdminBoard',[authJwt.isAdmin],controller.updateUser)
+  app.put('/api/admin/putUserAdminBoard',[authJwt.verifyToken, authJwt.isAdmin],controller.updateUser)
 };
