4555995c4b759ed18676c92798c52e23a26a6713
seitenca <alexdltg@protonmail.com>
2023-03-08 23:31:27 +0100

    update socketio server

--- labs/functions/socketio/index.js
+++ labs/functions/socketio/index.js
@@ -1,63 +1,52 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
+const server = require("http").Server(require("express"));
 const io = require("socket.io")(server);
 const port = process.env.WEBRTC_PORT;
-
-//const users = [];
-const players = [];
-
+var players = [];
+var mobs = [];
 
 server.listen(port, () => {
-  console.log(`Server socket.io running at ${port}`);
+  console.log(`Server running at ${port}`);
 });
 
 io.on('connection', function (socket) {
+  if (players.length == 0) socket.emit('host')
+  console.log("Player Connected!!");
+  socket.emit('socketID', { id: socket.id });
+  socket.emit('getPlayers', players);
 
+  socket.broadcast.emit('newPlayer', { id: socket.id });
 
-  socket.on('connection', (data) => {
-    if (players.length == 0) {
-      console.log("Host Connected!");
-      console.log(data.id);
-      players[0] = data.id;
-    }
-    else {
-      console.log("P2 Connected")
-      socket.broadcast.emit("getP2", { id: players.length });
-    }
-  })
-
-  socket.on('movement', (data) => {
-    console.log(data)
+  socket.on('playerMoved', function (data) {
+    data.id = socket.id;
+    socket.broadcast.emit('playerMoved', data);
     players.forEach(player => {
-      if (player == data.id) socket.emit('playerMoved', data);
-    })
-  })
-
-
-  /*
-
-  TODO: Retocar els disconects:  
-  He fet un petit disconnect mes endevant, esta super mal fet ho hem de retocar
-  
-  
-  socket.on('disconnectPlayer', (data) => {
-    console.log("Player disconnected");
-    for(i = 0; i <= players.length; i++){
-      if(data.id==players[i]){
-        players.splice(data.id , 1);
-        if(players.length){
-        for(i=0;i<=players.length; i++){
-          Console.log("Current players"+players.length)
-        }}
+      if (player.id == data.id) {
+        player.x = data.x;
+        player.y = data.y;
       }
-    }
-  })*/
+    })
+  });
+
+  socket.on('updateMobs', function (data) {
+    mobs = data;
+    socket.broadcast.emit('getMobs', mobs);
+  });
 
   socket.on('disconnect', function () {
-    console.log("Deleting playerInfo")
-    players.splice(0, players.length);
-  })
+    console.log("Player Disconnected");
+    socket.broadcast.emit('playerDisconnected', { id: socket.id });
+    for (let i = 0; i < players.length; i++) {
+      if (players[i].id == socket.id) {
+        players.splice(i, 1);
+      }
 
-
+    }
+  });
+  players.push(new player(socket.id, 0, 0));
 });
+
+function player(id, x, y) {
+  this.id = id;
+  this.x = x;
+  this.y = y;
+}
